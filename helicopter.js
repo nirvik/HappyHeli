e82de9d51051d6f93fd5ddfad2a52c942386ede9
nirvik <nirvik1993@gmail.com>
2014-06-15 16:04:43 -0400

    Environment not loading properly

--- helicopter.js
+++ helicopter.js
@@ -6,7 +6,10 @@
 	var height = canvas.height;
 
 	var player = {};
+	var environment = [];
 	var tick = 0;
+	var quadTree ;
+	var PlatformWidth = 32;
 
 	window.requestAnimFrame = function(){
 	    return (
@@ -112,17 +115,32 @@
 		this.y+=this.dy;
 	}
 
+	var walls = function(x,y){
+
+		this.type = "walls";
+		this.collidableWith = "player";
+		this.x = x;
+		this.y = y;
+
+		this.draw = function(){
+			ctx.rect(this.x,this,y,50,200);
+			ctx.stroke();
+		};
+	}
+
 	var player = function(player){
 
 		player.width = 50;
 		player.height = 50;
-		
+		player.type ="player";
+		player.collidableWith = "walls";
 		player.img = assetsLoaded.imgs["heli"];
 		player.img1 = assetsLoaded.imgs["heli2"];
 		player.image = new Image();
 		
 		player.dx = 0;
 		player.dy = 0;
+		player.speed = 6;
 
 		player.isThrust = false;
 		player.gravity = 1;
@@ -167,15 +185,197 @@
 		return player;
 	}(Object.create(Vector.prototype));
 
+	function QuadTree(boundBox,lvl){
+
+		this.bounds = boundBox || {
+			x :0,
+			y :0,
+			width :0,
+			height:0
+		};
+
+		var objects = [];
+		var maxLevel = 5;
+		var maxObjects = 10;
+
+		this.level = lvl || 0;
+		this.nodes = [];
+
+		this.clear = function(){
+			
+			objects = [];
+			for(var i=0;i<this.nodes.length;i++){
+				this.nodes[i].clear();
+			}
+
+			this.nodes = [];
+		};
+
+		this.getAllobjects = function(returnedobject){
+			for(var i=0;i<this.nodes.length;i++){
+				this.nodes[i].getAllobjects(returnedobject);
+			}
+
+			for(var i=0;i<objects.length;i++){
+				returnedobject.push(objects[i]);
+			}
+
+			return returnedobject;
+		};
+
+		this.findObjects = function(returnedobject,obj){
+
+			var index = this.getIndex(obj);
+			
+			if(index != -1 && this.nodes.length){
+				this.nodes[index].findObjects(returnedobject,obj);
+			}
+
+			for(var i=0;i<objects.length;i++){
+				returnedobject.push(objects[i]);
+			}
+
+			return returnedobject;
+		}
+
+		this.getIndex = function(obj){
+
+			var index = -1;
+
+			var verticalMidPoint = this.bounds.x + this.bounds.width/2;
+			var horizontMidPoint = this.bounds.y + this.bounds.height/2;
+			
+			var left = (obj.x < verticalMidPoint && obj.x+obj.width<verticalMidPoint);
+			var right = (obj.x >verticalMidPoint);
+
+			if(left){
+
+				var top =(obj.y<horizontMidPoint && obj.y+obj.height<horizontMidPoint);
+				var bottom = (obj.y>horizontMidPoint);
+
+				if(top){
+					index = 1;
+				}
+				else{
+					index = 2;
+				}
+			}
+
+			else{
+
+				var top =(obj.y<horizontMidPoint && obj.y+obj.height<horizontMidPoint);
+				var bottom = (obj.y>horizontMidPoint);
+
+				if(top){
+					index = 0;
+				}
+				else{
+					index = 3;
+				}
+			}
+
+			return index;
+		};
+
+		this.split = function(){
+
+			var subWidth = this.bounds.width/2 || 0;
+			var subHeight = this.bounds.height/2 || 0;
+			
+			this.nodes[0] = new QuadTree({
+				x:this.bounds.x+subWidth,
+				y:this.bounds.y,
+				width:subWidth,
+				height:subHeight
+			},level+1);
+			this.nodes[1] = new QuadTree({
+				x:this.bounds.x,
+				y:this.bounds.y,
+				width:subWidth,
+				height:subHeight
+			},level+1);
+			this.nodes[2] = new QuadTree({
+				x:this.bounds.x,
+				y:this.bounds.y+subHeight,
+				width:subWidth,
+				height:subHeight
+			},level+1);
+			this.nodes[3] = new QuadTree({
+				x:this.bounds.x+subWidth,
+				y:this.bounds.y+subHeight,
+				width:subWidth,
+				height:subHeight
+			},level+1);
+		};
+
+		this.insert = function(object){
+
+			if(typeof(object)=="undefined"){
+				return;
+			}
+
+			if(object instanceof Array){
+				for(var i=0;i<object.length;i++){
+					this.insert(object[i]);
+				}
+			}
+
+			if(this.nodes.length){
+				
+				var index = this.getIndex(object);
+				if(index != -1){
+					this.nodes[index].insert(object);
+				}
+			}
+
+			objects.push(object);
+
+			if(objects.length>maxObjects && this.level<maxLevel){
+				if(this.nodes[0] == null){
+					this.split();
+				}
+				var i = 0;
+				while(i<objects.length){
+
+					var index = this.getIndex(objects[i]); // Get the index of each element of the objects
+					if(index != -1){
+						this.nodes[index].insert((objects.splice(i,1))[0]);
+					}
+					else{
+						i++;
+					}
+				}
+			}
+		};
+
+	}
 
 	function animate(){
 		requestAnimFrame(animate);
 		ctx.clearRect(0,0,width,height);
+
+		for(var i=0;i<environment.length;i++){
+		//	environment[i].x -= player.speed;
+		//	environment[i].draw();
+			console.log(environment[i]);
+		}
+
+		/*
+		if(environment[0].x<= -PlatformWidth){
+			environment.shift();
+			environment.push(new walls(environment[environment.length-1].x+900,300));
+		}
+		*/
 		player.update();
 		player.draw();
 	}
 
 	function startGame(){
+
+		for(var i=1;i<Math.floor(canvas.width/PlatformWidth)+2 ;i++){
+			
+			environment[i] = new walls(i*100,200);
+		}
 		player.reset();
 		animate();
 	}
